5c6df8206112ceb2f41348bd8ba38ef6a67c04ac
Youdo Leam <Youdo@share.local>
2018-07-19 01:03:23 +0700

    Organize Model View Controller

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
-var itemArray = ["Find Mike","Buy Eggos","Destory Demogorgon"]
+var itemArray = [Item]()
 let defaults = UserDefaults.standard
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
-            itemArray = items
-        }
+        let newItem = Item()
+        newItem.title = "Go shooping"
+        itemArray.append(newItem)
+        
+        let newItem1 = Item()
+        newItem1.title = "Go clue"
+        itemArray.append(newItem1)
+        
+        let newItem2 = Item()
+        newItem2.title = "Reading"
+        itemArray.append(newItem2)
+//        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
+//            itemArray = items
+//        }
         }
        
 
@@ -29,22 +40,26 @@ let defaults = UserDefaults.standard
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        cell.accessoryType = itemArray[indexPath.row].done == true ? .checkmark : .none
         
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
-        {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else
-        {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark 
-        }
-        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+//        {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+//        }
+//        else
+//        {
+//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//        }
+//
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
@@ -55,8 +70,10 @@ let defaults = UserDefaults.standard
         
         let action = UIAlertAction(title: "Add Item", style: .default) {
             (action) in
-           self.itemArray.append(textField.text!)
-           self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            let newItem = Item()
+            newItem.title = textField.text!
+           self.itemArray.append(newItem)
+          // self.defaults.set(self.itemArray, forKey: "ToDoListArray")
            self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Youdo Leam on 19/7/18.
+//  Copyright © 2018 Youdo Leam. All rights reserved.
+//
+
+import Foundation
+class Item
+{
+    var title : String = ""
+    var done : Bool = false
+}
